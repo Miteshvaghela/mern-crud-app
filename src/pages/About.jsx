88e658fea2364d87a7f19c89ac9c39eefda426e0
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-06-18 13:09:35 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+
 const About = () => {
   return (
-    <Container fuild className='my-5'>
+    <Container fuild={+true} className='my-5'>
       <h1>About</h1>
     </Container>
   )
